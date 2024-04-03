37b973f4d98f9e58415d3afd234641ad21a4b5e1
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-04-02 20:47:23 -0700

    Apr2-Added a few themes

--- themes/themes.js
+++ themes/themes.js
@@ -1,1 +1,17 @@
 // themes for app
+
+export const colours = {
+    red: "#DB3939",
+    background: "#343434",
+    darkGrey: "#343434",
+    offWhite: "#E0E0E0",
+    mediumGray: "#74797F",
+}
+
+export const darkModeThemes = {
+    smallText: "#74797F",
+    icons: "#7f797F",
+    footerIcons: "#7F797F",
+    largeText: "#E0E0E0",
+    bigButton: "#DB3939",
+}
