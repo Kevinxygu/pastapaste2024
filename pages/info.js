abd2ed58931e7770dc35832efd3ed1752c0f9090
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-07-02 22:53:04 -0700

    added comments

--- pages/info.js
+++ pages/info.js
@@ -3,6 +3,7 @@ import 'next/link'
 import HeaderPage from '../components/global/HeaderPage'
 import Footer from '../components/global/Footer'
 
+// side page with instructions for the app
 export default function Info() {
     return (
       <div>
--- pages/settings.js
+++ pages/settings.js
@@ -2,6 +2,7 @@ import 'react'
 import HeaderPage from '../components/global/HeaderPage'
 import Footer from '../components/global/Footer'
 
+// side page with settings for the app, but currently it's just the settings page
 export default function Settings() {
     return (
       <div>
